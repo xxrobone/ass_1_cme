b8ef8dc7be61c258889e236eca33a37aa47c9766
robonexx <robertwagar@gmail.com>
2023-02-05 13:43:46 +0100

    nav hover effects

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const About = () => {
   return (
       <div>
-          <h1>ABOUT PAGE</h1>
+      <h1>About</h1>
+      <h4>This page is my assignments at CME education frontend app developer</h4>
+      <p>I will continuosly add my projects to this site, to test them out, already started with at crud
+        not an assignment but might use it some how during these weeks. 
+
+        Too many ideas, not enought time. 
+        
+      </p>
     </div>
   )
 }
