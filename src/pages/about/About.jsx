1e2c3a432b0198eb07afca9775ae576a0cd89a89
robonexx <robertwagar@gmail.com>
2023-02-05 15:27:49 +0100

    adding some text to one of the extra pages, about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
+//styles
+import './About.scss'
+
 export const About = () => {
   return (
-      <div>
+    <div className='about'>
+      <main>
       <h1>About</h1>
       <h4>This page is my assignments at CME education frontend app developer</h4>
       <p>I will continuosly add my projects to this site, to test them out, already started with at crud
         not an assignment but might use it some how during these weeks. 
 
         Too many ideas, not enought time. 
-        
+
       </p>
+
+      </main>
     </div>
   )
 }
